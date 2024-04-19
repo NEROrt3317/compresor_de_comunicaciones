a8782cb9e2dae9623583e5d45dcfe39e3388fdff
jorgebls <116990190+jorgebls@users.noreply.github.com>
2024-04-19 08:18:19 -0500

    Update interfaz.py

--- src/console/interfaz.py
+++ src/console/interfaz.py
@@ -4,88 +4,77 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.core.window import Window
-from kivy.graphics import Color, RoundedRectangle
 
 # Importa la clase CompresorZlib de tu módulo de compresión
 import sys
-sys.path.append("src")
-
-import compressor.compressorlogic as compresorlogic
-from compressor.compressorlogic import *
+sys.path.append("src")  # Añade el directorio 'src' a los directorios donde Python busca módulos
+import compressor.compressorlogic as compresorlogic  # Importa el módulo con la lógica de compresión
+from compressor.compressorlogic import *  # Importa todas las clases y funciones del módulo de lógica del compresor
 
 class MiAplicacion(App):
     def build(self):
-        Window.clearcolor = (0.06, 0.1, 0.12, 1)  # Un tono oscuro para el fondo de la ventana
+        Window.clearcolor = (0.06, 0.1, 0.12, 1)  # Establece el color de fondo de la ventana
 
-        # Layout principal
+        # Layout principal con orientación vertical
         layout = BoxLayout(orientation='vertical', padding=[10, 20], spacing=10)
 
-        # Título
+        # Título de la aplicación
         titulo = Label(text='Compresor de Comunicaciones', font_size='40sp', color=(0.2, 0.45, 0.8, 1), size_hint=(1, None), height=60)
 
-        # Descripción
+        # Descripción de la aplicación
         descripcion = Label(text=('El Compresor de Comunicaciones es una herramienta diseñada para optimizar la '
                                   'transmisión de datos al reducir el tamaño de los archivos sin comprometer la '
                                   'integridad de la información.'),
                             font_size='16sp', color=(0.7, 0.7, 0.7, 1), size_hint=(1, None), halign='left', valign='middle')
-        descripcion.bind(size=descripcion.setter('text_size'))  # Esto permite que el texto se ajuste al tamaño del Label
+        descripcion.bind(size=descripcion.setter('text_size'))  # Ajusta el tamaño del texto al tamaño del widget
 
-        # Añadir widgets al layout
-        layout.add_widget(titulo)
-        layout.add_widget(descripcion)
+        layout.add_widget(titulo)  # Añade el título al layout
+        layout.add_widget(descripcion)  # Añade la descripción al layout
 
-        # Ajustar la altura del layout de descripción para que todo el texto sea visible
-        descripcion.size_hint_y = None
-        descripcion.height = 150  # Ajusta esto según sea necesario para tu texto y tamaño de ventana
-
-
-        # Caja de entrada de texto
-        self.entrada = TextInput(hint_text='Escribe algo aquí o introduce el texto comprimido', font_size=25,size_hint=(2, 0.6), background_color=(0.12, 0.14, 0.17, 1), foreground_color=(0.8, 0.8, 0.8, 1))
+        # Input para texto a comprimir/descomprimir
+        self.entrada = TextInput(hint_text='Escribe algo aquí o introduce el texto comprimido', font_size=25, size_hint=(2, 0.6), background_color=(0.12, 0.14, 0.17, 1), foreground_color=(0.8, 0.8, 0.8, 1))
         layout.add_widget(self.entrada)
 
-        # Botones con esquinas redondeadas
+        # Layout para botones con acción de compresión y descompresión
         layout_botones = BoxLayout(size_hint=(1, 0.15), spacing=10)
         boton1 = Button(text='Comprimir', background_normal='', background_color=(0.2, 0.45, 0.8, 1), color=(1, 1, 1, 1))
         boton2 = Button(text='Descomprimir', background_normal='', background_color=(0.2, 0.45, 0.8, 1), color=(1, 1, 1, 1))
-        boton1.bind(on_press=self.comprimir)
-        boton2.bind(on_press=self.descomprimir)
+        boton1.bind(on_press=self.comprimir)  # Vincula el botón a la función comprimir
+        boton2.bind(on_press=self.descomprimir)  # Vincula el botón a la función descomprimir
         layout_botones.add_widget(boton1)
         layout_botones.add_widget(boton2)
         layout.add_widget(layout_botones)
 
-        # Caja de texto para mostrar el resultado
-        self.resultado = TextInput(text=' aca se motrara el resultado...',font_size=25)
+        # Input para mostrar resultados de la compresión/descompresión
+        self.resultado = TextInput(text='Aquí se mostrará el resultado...', font_size=25)
         layout.add_widget(self.resultado)
-
-
 
         # Inicializa la clase de compresión
         self.compresor = compresorlogic.CompresorZlib()
 
-        return layout
+        return layout  # Devuelve el layout principal de la aplicación
 
     def comprimir(self, instance):
+        # Método para comprimir texto
         if self.entrada.text == "":
-            self.resultado.text="Por favor, ingrese algún texto para comprimir."
+            self.resultado.text = "Por favor, ingrese algún texto para comprimir."
         else:
             try:
-                result=self.compresor.comprimir(self.entrada.text)
+                result = self.compresor.comprimir(self.entrada.text)
                 self.resultado.text = "Texto comprimido (en bytes): \n" + result.hex()
             except Exception as e:
-                self.resultado.text = f"texto inavalido para comprimir....."
+                self.resultado.text = "Texto inválido para comprimir..."
 
     def descomprimir(self, instance):
-
-        if self.entrada.text== "":
-            self.resultado.text="Por favor, ingrese algún texto en formato hexadecimal para descomprimir."
-
+        # Método para descomprimir texto
+        if self.entrada.text == "":
+            self.resultado.text = "Por favor, ingrese algún texto en formato hexadecimal para descomprimir."
         else:
-
             try:
-                result=self.compresor.descomprimir(self.entrada.text)
+                result = self.compresor.descomprimir(self.entrada.text)
                 self.resultado.text = "Texto descomprimido: \n" + result
             except Exception as e:
-                self.resultado.text = f"texto inavalido para descomprimir.\n Por favor, ingrese texto en formato hexadecimal para descomprimir."
+                self.resultado.text = "Texto inválido para descomprimir. Por favor, ingrese texto en formato hexadecimal para descomprimir."
 
 if __name__ == '__main__':
-    MiAplicacion().run()
+    MiAplicacion().run()  # Ejecuta la aplicación
